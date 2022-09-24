99f563ed7045136c03d9f5ed6baaab3906d450c6
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-24 16:18:31 -0400

    Add HttpClientModule import and to imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MatProgressBarModule} from '@angular/material/progress-bar';
 import { MatSelectModule} from '@angular/material/select'; 
 import {MatTableModule } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.modules';
 @NgModule({
   declarations: [
@@ -47,6 +48,7 @@ import { AppRoutingModule } from './app-routing.modules';
     MatBottomSheetModule,
     MatProgressBarModule,
     FormsModule,
+    HttpClientModule,
     MatSelectModule,
     MatCheckboxModule,
     MatRadioModule,
