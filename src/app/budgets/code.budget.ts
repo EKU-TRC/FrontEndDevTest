8a6a105f545cfd6149a0ca394202296dbd6bbd30
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-25 06:31:30 -0400

    Add getFiscalYear, getBudgetCode, and getBudgetTitle and their imports to code.budget.ts

--- src/app/budgets/code.budget.ts
+++ src/app/budgets/code.budget.ts
@@ -3,6 +3,9 @@ import { Observable } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Code } from "../interface/code";
 import { BudgetCodeId } from "../model/budget-code-id";
+import { FiscalYear } from "../model/fiscal-year";
+import { BudgetCode } from "../model/budget-code";
+import { BudgetTitle } from "../model/budget-title";
 
 @Injectable ({providedIn: 'root'})
 export class codeBudget
@@ -19,6 +22,21 @@ getCodes(): Observable<any>{
 
 getBudgetCodeId(): Observable<BudgetCodeId[]>{
     return this.httpclient.get<BudgetCodeId[]>('https://uat.trc.eku.edu/budgetcodeexam/api/id/', {
+        headers: this.httpOptions.headers
+    })
+}
+getFiscalYear(): Observable<FiscalYear[]>{
+    return this.httpclient.get<FiscalYear[]>('https://uat.trc.eku.edu/budgetcodeexam/api/year/', {
+        headers: this.httpOptions.headers
+    })
+}
+getBudgetCode(): Observable<BudgetCode[]>{
+    return this.httpclient.get<BudgetCode[]>('https://uat.trc.eku.edu/budgetcodeexam/api/code/', {
+        headers: this.httpOptions.headers
+    })
+}
+getBudgetTitle(): Observable<BudgetTitle[]>{
+    return this.httpclient.get<BudgetTitle[]>('https://uat.trc.eku.edu/budgetcodeexam/api/title/', {
         headers: this.httpOptions.headers
     })
 }
