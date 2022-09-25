b253de5fa4d9d35494098808c19432b23882f12b
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-25 13:06:09 -0400

    Add response: Response and import Response and add to ngOnInit in codes.component.ts. Add HTML coding for the table including conditions and loops to codes.component.html

--- src/app/component/codes/codes.component.ts
+++ src/app/component/codes/codes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { codeService } from 'src/app/services/code.service';
+import { Response } from 'src/app/interface/response.interface';
 
 @Component({
   selector: 'app-codes',
@@ -7,6 +8,7 @@ import { codeService } from 'src/app/services/code.service';
   styleUrls: ['./codes.component.css']
 })
 export class CodesComponent implements OnInit {
+  response!: Response; 
 
   constructor(private codeService: codeService) { }
 
@@ -14,6 +16,7 @@ export class CodesComponent implements OnInit {
     this.codeService.getCodes().subscribe(
       (results: any) => {
         console.log(results);
+        this.response = results;
       }
     );
   }
