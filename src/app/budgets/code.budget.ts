318187df9f23824ea408ee480658fa5cacf23ce6
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-25 06:09:10 -0400

    Add httpOptions and HttpHeaders import to the code.budget.ts file (in the budgets folder...should be called services but I know this convention now)

--- src/app/budgets/code.budget.ts
+++ src/app/budgets/code.budget.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Code } from "../interface/code";
 
 @Injectable ({providedIn: 'root'})
 export class codeBudget
 { constructor (private httpclient: HttpClient){}
+
+httpOptions = {
+    headers: new HttpHeaders({
+        'Content-type': 'application/json',
+    })
+}
 getCodes(): Observable<any>{
     return this.httpclient.get('https://uat.trc.eku.edu/budgetcodeexam/api/all/');
 }
