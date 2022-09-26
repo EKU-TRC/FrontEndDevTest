d9bea50d2d20ba71388c2d850dcc29708c84ec16
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-26 16:05:10 -0400

    Add reactive form coding to both codes.component.ts and html. Form goes to console and table appears

--- src/app/component/codes/codes.component.ts
+++ src/app/component/codes/codes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { codeService } from 'src/app/services/code.service';
 import { Response } from 'src/app/interface/response.interface';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, AbstractControl, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-codes',
@@ -10,15 +10,19 @@ import { FormBuilder } from '@angular/forms';
 })
 
 export class CodesComponent implements OnInit{
-  response: any;
+  public userForm:FormGroup;
   fiscalYear: string='';
   budgetTitle: string='';
-  
+  response: any;
 
   constructor(
     private codeService: codeService,
     private formBuilder: FormBuilder,
-    ) { }
+    ) { this.userForm=this.formBuilder.group({
+      fiscalYear:'',
+      budgetTitle:''
+    });
+  }
   
     ngOnInit(): void {
     this.codeService.getCodes().subscribe(
@@ -28,7 +32,8 @@ export class CodesComponent implements OnInit{
       }
     );
   }
-  onSubmit() {
-    console.log('Fiscal Year:',this.fiscalYear, 'Budget Title:',this.budgetTitle);
-  }
+  setValue() {
+    console.log(this.fiscalYear=this.userForm.get('fiscalYear')?.value)
+    console.log(this.budgetTitle=this.userForm.get('budgetTitle')?.value)
+}
 }
